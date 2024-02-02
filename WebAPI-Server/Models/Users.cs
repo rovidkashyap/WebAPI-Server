ec071b940f30aef09d283989354f3cdb2732378e
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-02-02 16:12:28 +0530

    Add project files.

---
+++ WebAPI-Server/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_Server.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string City { get; set; }
+    }
+}
